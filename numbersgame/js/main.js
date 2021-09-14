77dea236be1c3099959ce13eae08f3e24fa71002
yusuke <sato11.jp@gmail.com>
2021-09-14 22:50:02 +0900

    create numbersgame

---
+++ numbersgame/js/main.js
@@ -0,0 +1,54 @@
+'use strict';
+
+{
+  class Panel {
+    constructor() {
+      this.el = document.createElement('li');
+      this.el.classList.add('pressed');
+    }
+
+    getEl() {
+      return this.el;
+    }
+
+    activate(num) {
+      this.el.classList.remove('pressed');
+      this.el.textContent = num;
+    }
+  }
+
+  class Board {
+    constructor() {
+      this.panels = [];
+      for (let i = 0; i < 4; i++) {
+        this.panels.push(new Panel());
+      }
+      this.setup();
+    }
+
+    setup() {
+      const board = document.getElementById('board');
+      this.panels.forEach(panel => {
+        // board.appendChild(panel.el);
+        // 外部からプロパティアクセスしない方が良いので(カプセル化)
+        board.appendChild(panel.getEl());
+      });
+    }
+
+    activate() {
+      const nums = [0,1,2,3];
+
+      this.panels.forEach(panel => {
+        const num = nums.splice(Math.floor(Math.random() * nums.length), 1)[0];
+        panel.activate(num);
+      });
+    }
+  }
+
+  const board = new Board();
+
+  const btn = document.getElementById('btn');
+  btn.addEventListener('click', () => {
+    board.activate();
+  });
+}
