35bfc91ac426f04ab9b38e64f9591b90d75ad555
yusuke <sato11.jp@gmail.com>
2021-09-15 15:55:26 +0900

    created numbergame

--- numbersgame/js/main.js
+++ numbersgame/js/main.js
@@ -2,9 +2,13 @@
 
 {
   class Panel {
-    constructor() {
+    constructor(game) {
+      this.game = game;
       this.el = document.createElement('li');
       this.el.classList.add('pressed');
+      this.el.addEventListener('click', () => {
+        this.check();
+      });
     }
 
     getEl() {
@@ -15,13 +19,25 @@
       this.el.classList.remove('pressed');
       this.el.textContent = num;
     }
+
+    check() {
+      if (this.game.getCurrentNum() === parseInt(this.el.textContent, 10)) {
+        this.el.classList.add('pressed');
+        this.game.addCurrentNum();
+
+        if (this.game.getCurrentNum() === this.game.getLevel() ** 2) {
+          clearTimeout(this.game.getTimeoutId());
+        }
+      }
+    }
   }
 
   class Board {
-    constructor() {
+    constructor(game) {
+      this.game = game;
       this.panels = [];
-      for (let i = 0; i < 4; i++) {
-        this.panels.push(new Panel());
+      for (let i = 0; i < this.game.getLevel() ** 2; i++) {
+        this.panels.push(new Panel(this.game));
       }
       this.setup();
     }
@@ -36,19 +52,83 @@
     }
 
     activate() {
-      const nums = [0,1,2,3];
+      const nums = [];
+      for (let i = 0; i < this.game.getLevel() ** 2; i++) {
+        nums.push(i);
+      }
 
       this.panels.forEach(panel => {
-        const num = nums.splice(Math.floor(Math.random() * nums.length), 1)[0];
+        const num = nums.splice(Math.floor(Math.random() * nums.length), 1)[0]; //spliceの返値は配列
         panel.activate(num);
       });
     }
   }
 
-  const board = new Board();
 
-  const btn = document.getElementById('btn');
-  btn.addEventListener('click', () => {
-    board.activate();
-  });
+
+  class Game {
+    constructor(level) {
+      this.level = level;
+      this.board = new Board(this);
+
+      this.currentNum = undefined;
+      this.startTime = undefined;
+      this.timeoutId = undefined;
+
+      const btn = document.getElementById('btn');
+      btn.addEventListener('click', () => {
+        this.start();
+      });
+      this.setup();
+    }
+
+    setup() {
+      const container = document.getElementById('container');
+      const PANEL_WIDTH = 50;
+      const BOARD_PADDING = 10;
+      /* 50px * 2 + 10px * 2 */
+      container.style.width = PANEL_WIDTH * this.level + BOARD_PADDING * 2 + 'px';
+    }
+
+    start() {
+      if (typeof this.timeoutId !== 'undefined') {
+        clearTimeout(this.timeoutId);
+      }
+
+      this.currentNum = 0;
+      this.board.activate();
+
+      this.startTime = Date.now();
+      this.runTimer();
+    }
+
+    runTimer() {
+      const timer = document.getElementById('timer');
+      timer.textContent = ((Date.now() - this.startTime) / 1000).toFixed(2);
+
+      this.timeoutId = setTimeout(() => {
+        this.runTimer();
+      }, 10);
+    }
+
+    addCurrentNum() {
+      this.currentNum++;
+    }
+
+    getCurrentNum() {
+      return this.currentNum;
+    }
+
+    getTimeoutId() {
+      return this.timeoutId;
+    }
+
+    getLevel() {
+      return this.level;
+    }
+  }
+
+  new Game(5);
+
+
 }
